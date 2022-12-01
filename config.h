6ea2596a84d36aef06b3f67c110a8e48051fe59a
jacob_howard <jah0147@auburn.edu>
2022-12-01 13:47:21 -0600

    Added console clearing back into code

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 #define BLACKJACK_CONFIG_H
 
 //#define TESTING //comment out when not testing
-//#define ClearConsole //comment out if you do not want the console messages to ever clear
+#define ClearConsole //comment out if you do not want the console messages to ever clear
 //#define ResizeConsole //comment out to turn off auto-resizing of the console window
 #define DELAY //comment out if you want to disable delays (this may break some game functionality)
 //#define SPASHSCREEN //comment out if you do not want to see the text spash screen at the begining
